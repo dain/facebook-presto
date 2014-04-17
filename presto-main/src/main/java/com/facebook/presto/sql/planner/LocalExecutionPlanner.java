prestodb/presto

Rename HashJoinOperator => LookupJoinOperator

presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
@@ -27,12 +27,11 @@ import com.facebook.presto.operator.FilterFunctions;
 import com.facebook.presto.operator.HashAggregationOperator.HashAggregationOperatorFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashBuilderOperatorFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashSupplier;
-import com.facebook.presto.operator.HashJoinOperator;
-import com.facebook.presto.operator.HashJoinOperator.HashJoinOperatorFactory;
 import com.facebook.presto.operator.HashSemiJoinOperator.HashSemiJoinOperatorFactory;
 import com.facebook.presto.operator.InMemoryExchange;
 import com.facebook.presto.operator.InMemoryExchangeSourceOperator.InMemoryExchangeSourceOperatorFactory;
 import com.facebook.presto.operator.LimitOperator.LimitOperatorFactory;
+import com.facebook.presto.operator.LookupJoinOperator;
 import com.facebook.presto.operator.MarkDistinctOperator.MarkDistinctOperatorFactory;
 import com.facebook.presto.operator.MaterializeSampleOperator;
 import com.facebook.presto.operator.OperatorFactory;
@@ -122,6 +121,7 @@ import java.util.List;
 import java.util.Map;
 
 import static com.facebook.presto.operator.DistinctLimitOperator.DistinctLimitOperatorFactory;
+import static com.facebook.presto.operator.LookupJoinOperator.LookupJoinOperatorFactory;
 import static com.facebook.presto.operator.TableCommitOperator.TableCommitOperatorFactory;
 import static com.facebook.presto.operator.TableCommitOperator.TableCommitter;
 import static com.facebook.presto.operator.TableWriterOperator.TableWriterOperatorFactory;
@@ -845,7 +845,7 @@ public class LocalExecutionPlanner
             return new PhysicalOperation(operator, outputMappings.build(), probeSource);
         }
 
-        private HashJoinOperatorFactory createJoinOperator(
+        private LookupJoinOperatorFactory createJoinOperator(
                 JoinNode.Type type,
                 HashSupplier hashSupplier,
                 List<TupleInfo> probeTupleInfos,
@@ -854,10 +854,10 @@ public class LocalExecutionPlanner
         {
             switch (type) {
                 case INNER:
-                    return HashJoinOperator.innerJoin(context.getNextOperatorId(), hashSupplier, probeTupleInfos, probeJoinChannels);
+                    return LookupJoinOperator.innerJoin(context.getNextOperatorId(), hashSupplier, probeTupleInfos, probeJoinChannels);
                 case LEFT:
                 case RIGHT:
-                    return HashJoinOperator.outerJoin(context.getNextOperatorId(), hashSupplier, probeTupleInfos, probeJoinChannels);
+                    return LookupJoinOperator.outerJoin(context.getNextOperatorId(), hashSupplier, probeTupleInfos, probeJoinChannels);
                 default:
                     throw new UnsupportedOperationException("Unsupported join type: " + type);
             }

presto-main/src/test/java/com/facebook/presto/benchmark/HashBuildAndJoinBenchmark.java
@@ -16,8 +16,7 @@ package com.facebook.presto.benchmark;
 import com.facebook.presto.operator.Driver;
 import com.facebook.presto.operator.DriverFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashBuilderOperatorFactory;
-import com.facebook.presto.operator.HashJoinOperator;
-import com.facebook.presto.operator.HashJoinOperator.HashJoinOperatorFactory;
+import com.facebook.presto.operator.LookupJoinOperator;
 import com.facebook.presto.operator.NullOutputOperator.NullOutputOperatorFactory;
 import com.facebook.presto.operator.OperatorFactory;
 import com.facebook.presto.operator.TaskContext;
@@ -29,6 +28,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 import static com.facebook.presto.benchmark.BenchmarkQueryRunner.createLocalQueryRunner;
+import static com.facebook.presto.operator.LookupJoinOperator.LookupJoinOperatorFactory;
 import static com.facebook.presto.util.Threads.daemonThreadsNamed;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 
@@ -57,7 +57,7 @@ public class HashBuildAndJoinBenchmark
         // join
         OperatorFactory lineItemTableScan = createTableScanOperator(0, "lineitem", "orderkey", "quantity");
 
-        HashJoinOperatorFactory joinOperator = HashJoinOperator.innerJoin(1, hashBuilder.getHashSupplier(), lineItemTableScan.getTupleInfos(), Ints.asList(0));
+        LookupJoinOperatorFactory joinOperator = LookupJoinOperator.innerJoin(1, hashBuilder.getHashSupplier(), lineItemTableScan.getTupleInfos(), Ints.asList(0));
 
         NullOutputOperatorFactory output = new NullOutputOperatorFactory(2, joinOperator.getTupleInfos());
 

presto-main/src/test/java/com/facebook/presto/benchmark/HashJoinBenchmark.java
@@ -18,8 +18,7 @@ import com.facebook.presto.operator.DriverContext;
 import com.facebook.presto.operator.DriverFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashBuilderOperatorFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashSupplier;
-import com.facebook.presto.operator.HashJoinOperator;
-import com.facebook.presto.operator.HashJoinOperator.HashJoinOperatorFactory;
+import com.facebook.presto.operator.LookupJoinOperator;
 import com.facebook.presto.operator.NullOutputOperator.NullOutputOperatorFactory;
 import com.facebook.presto.operator.OperatorFactory;
 import com.facebook.presto.operator.TaskContext;
@@ -31,6 +30,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 import static com.facebook.presto.benchmark.BenchmarkQueryRunner.createLocalQueryRunner;
+import static com.facebook.presto.operator.LookupJoinOperator.LookupJoinOperatorFactory;
 import static com.facebook.presto.util.Threads.daemonThreadsNamed;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 
@@ -66,7 +66,7 @@ public class HashJoinBenchmark
 
         OperatorFactory lineItemTableScan = createTableScanOperator(0, "lineitem", "orderkey", "quantity");
 
-        HashJoinOperatorFactory joinOperator = HashJoinOperator.innerJoin(1, hashSupplier, lineItemTableScan.getTupleInfos(), Ints.asList(0));
+        LookupJoinOperatorFactory joinOperator = LookupJoinOperator.innerJoin(1, hashSupplier, lineItemTableScan.getTupleInfos(), Ints.asList(0));
 
         NullOutputOperatorFactory output = new NullOutputOperatorFactory(2, joinOperator.getTupleInfos());
 

presto-main/src/test/java/com/facebook/presto/operator/TestHashJoinOperator.java
@@ -16,7 +16,6 @@ package com.facebook.presto.operator;
 import com.facebook.presto.ExceededMemoryLimitException;
 import com.facebook.presto.execution.TaskId;
 import com.facebook.presto.operator.HashBuilderOperator.HashBuilderOperatorFactory;
-import com.facebook.presto.operator.HashJoinOperator.HashJoinOperatorFactory;
 import com.facebook.presto.sql.analyzer.Session;
 import com.facebook.presto.util.MaterializedResult;
 import com.google.common.collect.ImmutableList;
@@ -29,6 +28,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 
+import static com.facebook.presto.operator.LookupJoinOperator.LookupJoinOperatorFactory;
 import static com.facebook.presto.operator.OperatorAssertion.assertOperatorEquals;
 import static com.facebook.presto.operator.RowPagesBuilder.rowPagesBuilder;
 import static com.facebook.presto.tuple.TupleInfo.SINGLE_LONG;
@@ -82,7 +82,7 @@ public class TestHashJoinOperator
         List<Page> probeInput = rowPagesBuilder(SINGLE_VARBINARY, SINGLE_LONG, SINGLE_LONG)
                 .addSequencePage(1000, 0, 1000, 2000)
                 .build();
-        HashJoinOperatorFactory joinOperatorFactory = HashJoinOperator.innerJoin(
+        LookupJoinOperatorFactory joinOperatorFactory = LookupJoinOperator.innerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(SINGLE_VARBINARY, SINGLE_LONG, SINGLE_LONG),
@@ -141,7 +141,7 @@ public class TestHashJoinOperator
                 .row("a")
                 .row("b")
                 .build();
-        HashJoinOperatorFactory joinOperatorFactory = HashJoinOperator.innerJoin(
+        LookupJoinOperatorFactory joinOperatorFactory = LookupJoinOperator.innerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(SINGLE_VARBINARY),
@@ -187,7 +187,7 @@ public class TestHashJoinOperator
                 .row("b")
                 .row("c")
                 .build();
-        HashJoinOperatorFactory joinOperatorFactory = HashJoinOperator.innerJoin(
+        LookupJoinOperatorFactory joinOperatorFactory = LookupJoinOperator.innerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(SINGLE_VARBINARY),
@@ -234,7 +234,7 @@ public class TestHashJoinOperator
                 .row((String) null)
                 .row("c")
                 .build();
-        HashJoinOperatorFactory joinOperatorFactory = HashJoinOperator.innerJoin(
+        LookupJoinOperatorFactory joinOperatorFactory = LookupJoinOperator.innerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(SINGLE_VARBINARY),
@@ -275,7 +275,7 @@ public class TestHashJoinOperator
         List<Page> probeInput = rowPagesBuilder(SINGLE_VARBINARY, SINGLE_LONG, SINGLE_LONG)
                 .addSequencePage(15, 20, 1020, 2020)
                 .build();
-        HashJoinOperatorFactory joinOperatorFactory = HashJoinOperator.outerJoin(
+        LookupJoinOperatorFactory joinOperatorFactory = LookupJoinOperator.outerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(SINGLE_VARBINARY, SINGLE_LONG, SINGLE_LONG),
@@ -339,7 +339,7 @@ public class TestHashJoinOperator
                 .row("a")
                 .row("b")
                 .build();
-        HashJoinOperatorFactory joinOperatorFactory = HashJoinOperator.outerJoin(
+        LookupJoinOperatorFactory joinOperatorFactory = LookupJoinOperator.outerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(SINGLE_VARBINARY),
@@ -387,7 +387,7 @@ public class TestHashJoinOperator
                 .row("b")
                 .row("c")
                 .build();
-        HashJoinOperatorFactory joinOperatorFactory = HashJoinOperator.outerJoin(
+        LookupJoinOperatorFactory joinOperatorFactory = LookupJoinOperator.outerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(SINGLE_VARBINARY),
@@ -435,7 +435,7 @@ public class TestHashJoinOperator
                 .row((String) null)
                 .row("c")
                 .build();
-        HashJoinOperatorFactory joinOperatorFactory = HashJoinOperator.outerJoin(
+        LookupJoinOperatorFactory joinOperatorFactory = LookupJoinOperator.outerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(SINGLE_VARBINARY),

dain/facebook-presto

presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
@@ -27,7 +27,7 @@ import com.facebook.presto.operator.FilterFunctions;
 import com.facebook.presto.operator.HashAggregationOperator.HashAggregationOperatorFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashBuilderOperatorFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashSupplier;
-import com.facebook.presto.operator.HashJoinOperators;
+import com.facebook.presto.operator.LookupJoinOperators;
 import com.facebook.presto.operator.HashSemiJoinOperator.HashSemiJoinOperatorFactory;
 import com.facebook.presto.operator.InMemoryExchange;
 import com.facebook.presto.operator.InMemoryExchangeSourceOperator.InMemoryExchangeSourceOperatorFactory;
@@ -845,10 +845,10 @@ public class LocalExecutionPlanner
         {
             switch (type) {
                 case INNER:
-                    return HashJoinOperators.innerJoin(context.getNextOperatorId(), hashSupplier, probeTypes, probeJoinChannels);
+                    return LookupJoinOperators.innerJoin(context.getNextOperatorId(), hashSupplier, probeTypes, probeJoinChannels);
                 case LEFT:
                 case RIGHT:
-                    return HashJoinOperators.outerJoin(context.getNextOperatorId(), hashSupplier, probeTypes, probeJoinChannels);
+                    return LookupJoinOperators.outerJoin(context.getNextOperatorId(), hashSupplier, probeTypes, probeJoinChannels);
                 default:
                     throw new UnsupportedOperationException("Unsupported join type: " + type);
             }

presto-main/src/test/java/com/facebook/presto/benchmark/HashBuildAndJoinBenchmark.java
@@ -16,7 +16,7 @@ package com.facebook.presto.benchmark;
 import com.facebook.presto.operator.Driver;
 import com.facebook.presto.operator.DriverFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashBuilderOperatorFactory;
-import com.facebook.presto.operator.HashJoinOperators;
+import com.facebook.presto.operator.LookupJoinOperators;
 import com.facebook.presto.operator.NullOutputOperator.NullOutputOperatorFactory;
 import com.facebook.presto.operator.OperatorFactory;
 import com.facebook.presto.operator.TaskContext;
@@ -56,7 +56,7 @@ public class HashBuildAndJoinBenchmark
         // join
         OperatorFactory lineItemTableScan = createTableScanOperator(0, "lineitem", "orderkey", "quantity");
 
-        OperatorFactory joinOperator = HashJoinOperators.innerJoin(1, hashBuilder.getHashSupplier(), lineItemTableScan.getTypes(), Ints.asList(0));
+        OperatorFactory joinOperator = LookupJoinOperators.innerJoin(1, hashBuilder.getHashSupplier(), lineItemTableScan.getTypes(), Ints.asList(0));
 
         NullOutputOperatorFactory output = new NullOutputOperatorFactory(2, joinOperator.getTypes());
 

presto-main/src/test/java/com/facebook/presto/benchmark/HashJoinBenchmark.java
@@ -18,7 +18,7 @@ import com.facebook.presto.operator.DriverContext;
 import com.facebook.presto.operator.DriverFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashBuilderOperatorFactory;
 import com.facebook.presto.operator.HashBuilderOperator.HashSupplier;
-import com.facebook.presto.operator.HashJoinOperators;
+import com.facebook.presto.operator.LookupJoinOperators;
 import com.facebook.presto.operator.NullOutputOperator.NullOutputOperatorFactory;
 import com.facebook.presto.operator.OperatorFactory;
 import com.facebook.presto.operator.TaskContext;
@@ -65,7 +65,7 @@ public class HashJoinBenchmark
 
         OperatorFactory lineItemTableScan = createTableScanOperator(0, "lineitem", "orderkey", "quantity");
 
-        OperatorFactory joinOperator = HashJoinOperators.innerJoin(1, hashSupplier, lineItemTableScan.getTypes(), Ints.asList(0));
+        OperatorFactory joinOperator = LookupJoinOperators.innerJoin(1, hashSupplier, lineItemTableScan.getTypes(), Ints.asList(0));
 
         NullOutputOperatorFactory output = new NullOutputOperatorFactory(2, joinOperator.getTypes());
 

presto-main/src/test/java/com/facebook/presto/operator/TestHashJoinOperator.java
@@ -81,7 +81,7 @@ public class TestHashJoinOperator
         List<Page> probeInput = rowPagesBuilder(VARCHAR, BIGINT, BIGINT)
                 .addSequencePage(1000, 0, 1000, 2000)
                 .build();
-        OperatorFactory joinOperatorFactory = HashJoinOperators.innerJoin(
+        OperatorFactory joinOperatorFactory = LookupJoinOperators.innerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(VARCHAR, BIGINT, BIGINT),
@@ -135,7 +135,7 @@ public class TestHashJoinOperator
                 .row("a")
                 .row("b")
                 .build();
-        OperatorFactory joinOperatorFactory = HashJoinOperators.innerJoin(
+        OperatorFactory joinOperatorFactory = LookupJoinOperators.innerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(VARCHAR),
@@ -181,7 +181,7 @@ public class TestHashJoinOperator
                 .row("b")
                 .row("c")
                 .build();
-        OperatorFactory joinOperatorFactory = HashJoinOperators.innerJoin(
+        OperatorFactory joinOperatorFactory = LookupJoinOperators.innerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(VARCHAR),
@@ -228,7 +228,7 @@ public class TestHashJoinOperator
                 .row((String) null)
                 .row("c")
                 .build();
-        OperatorFactory joinOperatorFactory = HashJoinOperators.innerJoin(
+        OperatorFactory joinOperatorFactory = LookupJoinOperators.innerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(VARCHAR),
@@ -269,7 +269,7 @@ public class TestHashJoinOperator
         List<Page> probeInput = rowPagesBuilder(VARCHAR, BIGINT, BIGINT)
                 .addSequencePage(15, 20, 1020, 2020)
                 .build();
-        OperatorFactory joinOperatorFactory = HashJoinOperators.outerJoin(
+        OperatorFactory joinOperatorFactory = LookupJoinOperators.outerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(VARCHAR, BIGINT, BIGINT),
@@ -328,7 +328,7 @@ public class TestHashJoinOperator
                 .row("a")
                 .row("b")
                 .build();
-        OperatorFactory joinOperatorFactory = HashJoinOperators.outerJoin(
+        OperatorFactory joinOperatorFactory = LookupJoinOperators.outerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(VARCHAR),
@@ -376,7 +376,7 @@ public class TestHashJoinOperator
                 .row("b")
                 .row("c")
                 .build();
-        OperatorFactory joinOperatorFactory = HashJoinOperators.outerJoin(
+        OperatorFactory joinOperatorFactory = LookupJoinOperators.outerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(VARCHAR),
@@ -424,7 +424,7 @@ public class TestHashJoinOperator
                 .row((String) null)
                 .row("c")
                 .build();
-        OperatorFactory joinOperatorFactory = HashJoinOperators.outerJoin(
+        OperatorFactory joinOperatorFactory = LookupJoinOperators.outerJoin(
                 0,
                 hashBuilderOperatorFactory.getHashSupplier(),
                 ImmutableList.of(VARCHAR),