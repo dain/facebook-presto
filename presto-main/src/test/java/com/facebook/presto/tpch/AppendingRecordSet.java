prestodb/presto

Add unit tests for index joins

presto-main/src/test/java/com/facebook/presto/tpch/AppendingRecordSet.java
@@ -1,0 +1,185 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tpch;
+
+import com.facebook.presto.spi.ColumnType;
+import com.facebook.presto.spi.RecordCursor;
+import com.facebook.presto.spi.RecordSet;
+import com.google.common.collect.ImmutableList;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkPositionIndex;
+
+public class AppendingRecordSet
+        implements RecordSet
+{
+    private final RecordSet delegate;
+    private final List<Object> appendedValues;
+    private final List<ColumnType> appendedTypes;
+
+    public AppendingRecordSet(RecordSet delegate, List<Object> appendedValues, List<ColumnType> appendedTypes)
+    {
+        this.delegate = checkNotNull(delegate, "delegate is null");
+        this.appendedValues = new ArrayList<>(checkNotNull(appendedValues, "appendedValues is null")); // May contain null elements
+        this.appendedTypes = ImmutableList.copyOf(checkNotNull(appendedTypes, "appendedTypes is null"));
+        checkArgument(appendedValues.size() == appendedTypes.size(), "appendedValues must have the same size as appendedTypes");
+        for (int i = 0; i < appendedValues.size(); i++) {
+            Object value = appendedValues.get(i);
+            if (value != null) {
+                checkArgument(appendedTypes.get(i).getNativeType().isInstance(value), "Object value does not match declared type");
+            }
+        }
+    }
+
+    @Override
+    public List<ColumnType> getColumnTypes()
+    {
+        return ImmutableList.<ColumnType>builder()
+                .addAll(delegate.getColumnTypes())
+                .addAll(appendedTypes)
+                .build();
+    }
+
+    @Override
+    public RecordCursor cursor()
+    {
+        return new AppendingRecordCursor(delegate.cursor(), delegate.getColumnTypes().size(), appendedValues, appendedTypes);
+    }
+
+    private static class AppendingRecordCursor
+            implements RecordCursor
+    {
+        private final RecordCursor delegate;
+        private final int delegateFieldCount;
+        private final List<Object> appendedValues;
+        private final List<ColumnType> appendedTypes;
+
+        private AppendingRecordCursor(RecordCursor delegate, int delegateFieldCount, List<Object> appendedValues, List<ColumnType> appendedTypes)
+        {
+            this.delegate = checkNotNull(delegate, "delegate is null");
+            this.delegateFieldCount = delegateFieldCount;
+            checkArgument(delegateFieldCount >= 0, "delegateFieldCount must be greater than or equal to zero");
+            this.appendedValues = checkNotNull(appendedValues, "appendedValues is null"); // May contain null elements
+            this.appendedTypes = ImmutableList.copyOf(checkNotNull(appendedTypes, "appendedTypes is null"));
+            checkArgument(appendedValues.size() == appendedTypes.size(), "appendedValues must have the same size as appendedTypes");
+        }
+
+        @Override
+        public long getTotalBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getCompletedBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getReadTimeNanos()
+        {
+            return 0;
+        }
+
+        @Override
+        public ColumnType getType(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getType(field);
+            }
+            else {
+                return appendedTypes.get(field - delegateFieldCount);
+            }
+        }
+
+        @Override
+        public boolean advanceNextPosition()
+        {
+            return delegate.advanceNextPosition();
+        }
+
+        @Override
+        public boolean getBoolean(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getBoolean(field);
+            }
+            else {
+                return (Boolean) appendedValues.get(field - delegateFieldCount);
+            }
+        }
+
+        @Override
+        public long getLong(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getLong(field);
+            }
+            else {
+                return (Long) appendedValues.get(field - delegateFieldCount);
+            }
+        }
+
+        @Override
+        public double getDouble(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getDouble(field);
+            }
+            else {
+                return (Double) appendedValues.get(field - delegateFieldCount);
+            }
+        }
+
+        @Override
+        public byte[] getString(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getString(field);
+            }
+            else {
+                return ((String) appendedValues.get(field - delegateFieldCount)).getBytes(StandardCharsets.UTF_8);
+            }
+        }
+
+        @Override
+        public boolean isNull(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.isNull(field);
+            }
+            else {
+                return appendedValues.get(field - delegateFieldCount) == null;
+            }
+        }
+
+        @Override
+        public void close()
+        {
+            delegate.close();
+        }
+    }
+}

presto-main/src/test/java/com/facebook/presto/tpch/ConcatRecordSet.java
@@ -1,0 +1,161 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tpch;
+
+import com.facebook.presto.spi.ColumnType;
+import com.facebook.presto.spi.RecordCursor;
+import com.facebook.presto.spi.RecordSet;
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkPositionIndex;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.transform;
+
+public class ConcatRecordSet
+        implements RecordSet
+{
+    private final Iterable<RecordSet> recordSets;
+    private final List<ColumnType> types;
+
+    public ConcatRecordSet(Iterable<RecordSet> recordSets, List<ColumnType> types)
+    {
+        this.recordSets = checkNotNull(recordSets, "recordSets is null");
+        this.types = ImmutableList.copyOf(checkNotNull(types, "types is null"));
+    }
+
+    @Override
+    public List<ColumnType> getColumnTypes()
+    {
+        return types;
+    }
+
+    @Override
+    public RecordCursor cursor()
+    {
+        Iterable<RecordCursor> recordCursors = transform(recordSets, new Function<RecordSet, RecordCursor>()
+        {
+            @Override
+            public RecordCursor apply(RecordSet recordSet)
+            {
+                checkState(recordSet.getColumnTypes().equals(types), "RecordSet types do not match declared types");
+                return recordSet.cursor();
+            }
+        });
+        return new ConcatRecordCursor(recordCursors.iterator(), types);
+    }
+
+    private static class ConcatRecordCursor
+            implements RecordCursor
+    {
+        private final Iterator<RecordCursor> iterator;
+        private final List<ColumnType> types;
+
+        private RecordCursor currentCursor;
+
+        private ConcatRecordCursor(Iterator<RecordCursor> iterator, List<ColumnType> types)
+        {
+            this.iterator = checkNotNull(iterator, "iterator is null");
+            this.types = ImmutableList.copyOf(checkNotNull(types, "types is null"));
+        }
+
+        @Override
+        public long getTotalBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getCompletedBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getReadTimeNanos()
+        {
+            return 0;
+        }
+
+        @Override
+        public ColumnType getType(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return types.get(field);
+        }
+
+        @Override
+        public boolean advanceNextPosition()
+        {
+            while (currentCursor == null || !currentCursor.advanceNextPosition()) {
+                if (!iterator.hasNext()) {
+                    return false;
+                }
+                if (currentCursor != null) {
+                    currentCursor.close();
+                }
+                currentCursor = iterator.next();
+            }
+            return true;
+        }
+
+        @Override
+        public boolean getBoolean(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.getBoolean(field);
+        }
+
+        @Override
+        public long getLong(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.getLong(field);
+        }
+
+        @Override
+        public double getDouble(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.getDouble(field);
+        }
+
+        @Override
+        public byte[] getString(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.getString(field);
+        }
+
+        @Override
+        public boolean isNull(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.isNull(field);
+        }
+
+        @Override
+        public void close()
+        {
+            if (currentCursor != null) {
+                currentCursor.close();
+            }
+            // Remaining iterator are lazily generated
+        }
+    }
+}

dain/facebook-presto

presto-main/src/test/java/com/facebook/presto/tpch/AppendingRecordSet.java
@@ -1,0 +1,185 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tpch;
+
+import com.facebook.presto.spi.RecordCursor;
+import com.facebook.presto.spi.RecordSet;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.collect.ImmutableList;
+import io.airlift.slice.Slice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkPositionIndex;
+
+public class AppendingRecordSet
+        implements RecordSet
+{
+    private final RecordSet delegate;
+    private final List<Object> appendedValues;
+    private final List<Type> appendedTypes;
+
+    public AppendingRecordSet(RecordSet delegate, List<Object> appendedValues, List<Type> appendedTypes)
+    {
+        this.delegate = checkNotNull(delegate, "delegate is null");
+        this.appendedValues = new ArrayList<>(checkNotNull(appendedValues, "appendedValues is null")); // May contain null elements
+        this.appendedTypes = ImmutableList.copyOf(checkNotNull(appendedTypes, "appendedTypes is null"));
+        checkArgument(appendedValues.size() == appendedTypes.size(), "appendedValues must have the same size as appendedTypes");
+        for (int i = 0; i < appendedValues.size(); i++) {
+            Object value = appendedValues.get(i);
+            if (value != null) {
+                checkArgument(appendedTypes.get(i).getJavaType().isInstance(value), "Object value does not match declared type");
+            }
+        }
+    }
+
+    @Override
+    public List<Type> getColumnTypes()
+    {
+        return ImmutableList.<Type>builder()
+                .addAll(delegate.getColumnTypes())
+                .addAll(appendedTypes)
+                .build();
+    }
+
+    @Override
+    public RecordCursor cursor()
+    {
+        return new AppendingRecordCursor(delegate.cursor(), delegate.getColumnTypes().size(), appendedValues, appendedTypes);
+    }
+
+    private static class AppendingRecordCursor
+            implements RecordCursor
+    {
+        private final RecordCursor delegate;
+        private final int delegateFieldCount;
+        private final List<Object> appendedValues;
+        private final List<Type> appendedTypes;
+
+        private AppendingRecordCursor(RecordCursor delegate, int delegateFieldCount, List<Object> appendedValues, List<Type> appendedTypes)
+        {
+            this.delegate = checkNotNull(delegate, "delegate is null");
+            this.delegateFieldCount = delegateFieldCount;
+            checkArgument(delegateFieldCount >= 0, "delegateFieldCount must be greater than or equal to zero");
+            this.appendedValues = checkNotNull(appendedValues, "appendedValues is null"); // May contain null elements
+            this.appendedTypes = ImmutableList.copyOf(checkNotNull(appendedTypes, "appendedTypes is null"));
+            checkArgument(appendedValues.size() == appendedTypes.size(), "appendedValues must have the same size as appendedTypes");
+        }
+
+        @Override
+        public long getTotalBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getCompletedBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getReadTimeNanos()
+        {
+            return 0;
+        }
+
+        @Override
+        public Type getType(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getType(field);
+            }
+            else {
+                return appendedTypes.get(field - delegateFieldCount);
+            }
+        }
+
+        @Override
+        public boolean advanceNextPosition()
+        {
+            return delegate.advanceNextPosition();
+        }
+
+        @Override
+        public boolean getBoolean(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getBoolean(field);
+            }
+            else {
+                return (Boolean) appendedValues.get(field - delegateFieldCount);
+            }
+        }
+
+        @Override
+        public long getLong(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getLong(field);
+            }
+            else {
+                return (Long) appendedValues.get(field - delegateFieldCount);
+            }
+        }
+
+        @Override
+        public double getDouble(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getDouble(field);
+            }
+            else {
+                return (Double) appendedValues.get(field - delegateFieldCount);
+            }
+        }
+
+        @Override
+        public byte[] getString(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.getString(field);
+            }
+            else {
+                return ((Slice) appendedValues.get(field - delegateFieldCount)).getBytes();
+            }
+        }
+
+        @Override
+        public boolean isNull(int field)
+        {
+            checkPositionIndex(field, delegateFieldCount + appendedTypes.size());
+            if (field < delegateFieldCount) {
+                return delegate.isNull(field);
+            }
+            else {
+                return appendedValues.get(field - delegateFieldCount) == null;
+            }
+        }
+
+        @Override
+        public void close()
+        {
+            delegate.close();
+        }
+    }
+}

presto-main/src/test/java/com/facebook/presto/tpch/ConcatRecordSet.java
@@ -1,0 +1,161 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tpch;
+
+import com.facebook.presto.spi.RecordCursor;
+import com.facebook.presto.spi.RecordSet;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkPositionIndex;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.transform;
+
+public class ConcatRecordSet
+        implements RecordSet
+{
+    private final Iterable<RecordSet> recordSets;
+    private final List<Type> types;
+
+    public ConcatRecordSet(Iterable<RecordSet> recordSets, List<Type> types)
+    {
+        this.recordSets = checkNotNull(recordSets, "recordSets is null");
+        this.types = ImmutableList.copyOf(checkNotNull(types, "types is null"));
+    }
+
+    @Override
+    public List<Type> getColumnTypes()
+    {
+        return types;
+    }
+
+    @Override
+    public RecordCursor cursor()
+    {
+        Iterable<RecordCursor> recordCursors = transform(recordSets, new Function<RecordSet, RecordCursor>()
+        {
+            @Override
+            public RecordCursor apply(RecordSet recordSet)
+            {
+                checkState(recordSet.getColumnTypes().equals(types), "RecordSet types do not match declared types");
+                return recordSet.cursor();
+            }
+        });
+        return new ConcatRecordCursor(recordCursors.iterator(), types);
+    }
+
+    private static class ConcatRecordCursor
+            implements RecordCursor
+    {
+        private final Iterator<RecordCursor> iterator;
+        private final List<Type> types;
+
+        private RecordCursor currentCursor;
+
+        private ConcatRecordCursor(Iterator<RecordCursor> iterator, List<Type> types)
+        {
+            this.iterator = checkNotNull(iterator, "iterator is null");
+            this.types = ImmutableList.copyOf(checkNotNull(types, "types is null"));
+        }
+
+        @Override
+        public long getTotalBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getCompletedBytes()
+        {
+            return 0;
+        }
+
+        @Override
+        public long getReadTimeNanos()
+        {
+            return 0;
+        }
+
+        @Override
+        public Type getType(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return types.get(field);
+        }
+
+        @Override
+        public boolean advanceNextPosition()
+        {
+            while (currentCursor == null || !currentCursor.advanceNextPosition()) {
+                if (!iterator.hasNext()) {
+                    return false;
+                }
+                if (currentCursor != null) {
+                    currentCursor.close();
+                }
+                currentCursor = iterator.next();
+            }
+            return true;
+        }
+
+        @Override
+        public boolean getBoolean(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.getBoolean(field);
+        }
+
+        @Override
+        public long getLong(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.getLong(field);
+        }
+
+        @Override
+        public double getDouble(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.getDouble(field);
+        }
+
+        @Override
+        public byte[] getString(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.getString(field);
+        }
+
+        @Override
+        public boolean isNull(int field)
+        {
+            checkPositionIndex(field, types.size());
+            return currentCursor.isNull(field);
+        }
+
+        @Override
+        public void close()
+        {
+            if (currentCursor != null) {
+                currentCursor.close();
+            }
+            // Remaining iterator are lazily generated
+        }
+    }
+}